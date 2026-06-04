5b927c6812028eb7e53c98de7b553e5d9008c473
lalalune <shawmakesmagic@gmail.com>
2026-06-04 09:26:17 -0700

    Define batched FRI general input relation

--- ArkLib/ProofSystem/BatchedFri/Spec/General.lean
+++ ArkLib/ProofSystem/BatchedFri/Spec/General.lean
@@ -38,11 +38,12 @@ variable (l m : ℕ)
 variable {ω : ReedSolomon.SmoothCosetFftDomain n F}
 
 -- /- Input/Output relations for the Batched FRI protocol. -/
-def inputRelation [DecidableEq F] (δ : ℝ≥0) :
+def inputRelation [DecidableEq F] (_δ : ℝ≥0) :
     Set
       (
         Unit × (∀ j, OracleStatement m ω j) × (Witness F s d m)
-      ) := sorry
+      ) :=
+  { input | BatchingRound.inputRelation s d m ((input.1, input.2.1), input.2.2) }
 
 
 /- Lifting FRI to include using `liftingLens`:
