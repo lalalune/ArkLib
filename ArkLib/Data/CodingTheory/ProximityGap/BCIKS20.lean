530276105ed87938916d05d4841a3d681287376b
Shaw <shawmakesmagic@gmail.com>
2026-06-06 03:58:42 -0600

    Add BCIKS20 proximity umbrella module

---
+++ ArkLib/Data/CodingTheory/ProximityGap/BCIKS20.lean
@@ -0,0 +1,35 @@
+/-
+Copyright (c) 2026 ArkLib Contributors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+-/
+
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.AffineLines.BWMatrix
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.AffineLines.GoodCoeffs
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.AffineLines.JointAgreement
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.AffineLines.Main
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.AffineLines.UniqueDecoding
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.AffineSpaces
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.AlphaWeight
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.Curves
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.Curves.Assembly
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.Curves.GoodCoeffs
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.Curves.JointAgreement
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.Curves.UniqueDecoding
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.ErrorBound
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.GammaGenuine
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.HenselNumerator
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.ListDecoding.Agreement
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.ListDecoding.CurvesBridge
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.ListDecoding.Extraction
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.ListDecoding.Guruswami
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.ListDecoding.RootClearing
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.P1Conditional
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.P2Close
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.P2Match
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.P2Reindex
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.P2Vanish
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.Prelude
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.ReedSolomonGap
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.S5Genuine
+import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.WeightedAgreement
+
