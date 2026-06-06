d4a22842f962b12828f01747333a75fb600c607d
lalalune <shawmakesmagic@gmail.com>
2026-06-06 08:45:54 -0700

    Update ArkLib key lemma

--- ArkLib/OracleReduction/FiatShamir/DuplexSponge/Security/KeyLemma.lean
+++ ArkLib/OracleReduction/FiatShamir/DuplexSponge/Security/KeyLemma.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2026 ArkLib Contributors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: ArkLib Contributors
+-/
+
 import ArkLib.OracleReduction.FiatShamir.Basic
 import ArkLib.OracleReduction.FiatShamir.DuplexSponge.Defs
 
