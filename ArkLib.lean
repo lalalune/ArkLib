373e81ea1833f9c6f900682e88f87dd3a178eb09
lalalune <shawmakesmagic@gmail.com>
2026-06-07 12:53:49 -0700

    feat(#140): expose zero-code MCA guardrail

--- ArkLib.lean
+++ ArkLib.lean
@@ -331,6 +331,7 @@ import ArkLib.Data.CodingTheory.ProximityGap.MCAMiddleBand
 import ArkLib.Data.CodingTheory.ProximityGap.MCAPlateauWindow
 import ArkLib.Data.CodingTheory.ProximityGap.MCASecondMoment
 import ArkLib.Data.CodingTheory.ProximityGap.MCAZeroCodeExact
+import ArkLib.Data.CodingTheory.ProximityGap.MCAZeroCodeGuardrail
 import ArkLib.Data.CodingTheory.ProximityGap.MuTwoPowDerandRIMRank
 import ArkLib.Data.CodingTheory.ProximityGap.MuTwoPowDerandRefutation
 import ArkLib.Data.CodingTheory.ProximityGap.MultiplicativeRigidityFiber
