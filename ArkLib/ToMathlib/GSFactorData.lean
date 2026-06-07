ed2362d2fcb90e4024a9fe3caa9838fe23c583b5
lalalune <shawmakesmagic@gmail.com>
2026-06-06 20:18:45 -0700

    autosync: ArkLib updates

--- ArkLib/ToMathlib/GSFactorData.lean
+++ ArkLib/ToMathlib/GSFactorData.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: ArkLib Contributors
 -/
 import ArkLib.ToMathlib.CorrelatedAgreementListDecodingClosed
+import ArkLib.ToMathlib.HPzBridge
 import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.ListDecoding.Agreement
 
 /-!
