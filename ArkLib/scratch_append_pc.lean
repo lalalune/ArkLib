9060eb4b73288126fad706785e87e374a30b747d
Shaw <shawmakesmagic@gmail.com>
2026-06-08 02:57:02 -0600

    auto-sync: post-push-race cleanup 2026-06-08T08:55:05Z

--- ArkLib/scratch_append_pc.lean
+++ ArkLib/scratch_append_pc.lean
@@ -14,6 +14,14 @@ theorem mem_support_run_of_prover_verifier
   unfold Reduction.run
   simp only [OptionT.run_bind, Option.elimM, bind_assoc, mem_support_bind_iff]
   refine ⟨some (tr, prv), ?_, ?_⟩
-  · revert hP; extract_goal; sorry
-  · sorry
+  · show some (tr, prv) ∈ support (some <$> R.prover.run stmt wit)
+    simp only [support_map, Set.mem_image, Option.some.injEq]
+    exact ⟨_, hP, rfl⟩
+  · simp only [Option.elim, liftM_bind, mem_support_bind_iff, Option.getM_some,
+      OptionT.run_pure, bind_pure_comp, map_bind, map_pure, support_bind, Set.mem_iUnion]
+    refine ⟨some vout, ?_, ?_⟩
+    · show some vout ∈ support (some <$> OptionT.run (R.verifier.run stmt tr))
+      simp only [support_map, Set.mem_image, Option.some.injEq]
+      exact ⟨_, hV, rfl⟩
+    · simp [Option.getM_some, support_pure]
 end Reduction
