05ce076710caf34a4356628efe292fd7b5064325
Katy Hristova <156920630+katyhr@users.noreply.github.com>
2026-06-11 11:38:40 +0100

    MCA generators lemmas (#489)
    * port prelims from pr
    
    * port basic from pr
    
    * changes from PR
    
    * new dependencies
    
    * lin combo lemmas
    
    * defs from section 3
    
    * ArkLib/Data/CodingTheory/Basic/MDSCode.lean
    
    * minor edits
    
    * SZ counting form for rmk 320
    
    * remove lint errors
    
    * one more lint error
    
    * tensor gen def
    
    * tensor of gens
    
    * lemma 4.1 cor 4.2
    
    * update dependencies
    
    * broken proof drafts
    
    * next draft
    
    * lemmas from section 4_1
    
    * update dependencies
    
    * linear combos in projected codes
    
    * tensor gens def
    
    * pseudoinverses
    
    * monotonicity lemma
    
    * small edits
    
    * change S S' to general types
    
    * remove monotonicity lemma
    
    * small changes
    
    * update imports
    
    * optimise proofs
    
    * some edits
    
    * remove set
    option linter
    
    * small proof optimisation
    
    * some more changes
    
    * some cleanup
    
    * fix build error
    
    * second tensor def
    
    * two versions of the tensor gen proved equivalent
    
    * reduce line length
    
    ---------
    
    Co-authored-by: Alexander Hicks <25369263+alexanderlhicks@users.noreply.github.com>

--- ArkLib.lean
+++ ArkLib.lean
@@ -76,6 +76,7 @@ import ArkLib.Data.CodingTheory.ProximityGap.DG25.Basic
 import ArkLib.Data.CodingTheory.ProximityGap.DG25.MainResults
 import ArkLib.Data.CodingTheory.ProximityGap.DG25.ReedSolomon
 import ArkLib.Data.CodingTheory.ProximityGap.Folding
+import ArkLib.Data.CodingTheory.ProximityGap.MCAGenerator
 import ArkLib.Data.CodingTheory.ProximityGap.ProximityGenerators
 import ArkLib.Data.CodingTheory.ReedSolomon
 import ArkLib.Data.CodingTheory.ReedSolomon.Multilinear
