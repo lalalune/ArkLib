dd157cc43e1177ccd053a6042663396266079594
lekt8 <lewistham9x@gmail.com>
2026-06-12 10:31:14 +0800

    chore: update Johnson count imports (#372)

--- ArkLib/Data/CodingTheory/ProximityGap/RegimeIIBridge.lean
+++ ArkLib/Data/CodingTheory/ProximityGap/RegimeIIBridge.lean
@@ -3,7 +3,7 @@ Copyright (c) 2026 ArkLib Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: ArkLib Contributors
 -/
-import ArkLib.Data.CodingTheory.ProximityGap.«141JohnsonCount»
+import ArkLib.Data.CodingTheory.ProximityGap.MCAJohnsonCount
 
 /-!
 # Regimes (I)+(II) of `InteriorCeiling` modularized onto ONE named line-close bound (#357)
