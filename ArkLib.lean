3fcba95a863f7e19a45b655ac870c70d2da6a983
lekt8 <lewistham9x@gmail.com>
2026-06-12 11:10:03 +0800

    Fix Binius bad sumcheck probability bound (#374)

--- ArkLib.lean
+++ ArkLib.lean
@@ -1219,6 +1219,7 @@ import ArkLib.Data.CodingTheory.ProximityGap.TruncatedRecurrenceTransversality
 import ArkLib.Data.CodingTheory.ProximityGap.TwoGenPackingCapacity
 import ArkLib.Data.CodingTheory.ProximityGap.TwoLineExtraction
 import ArkLib.Data.CodingTheory.ProximityGap.TwoPlusAntipodalChordLaw
+import ArkLib.Data.CodingTheory.ProximityGap.TwoPowerSubsetSumSpectrum
 import ArkLib.Data.CodingTheory.ProximityGap.TwoPrimeSyndromeBudget
 import ArkLib.Data.CodingTheory.ProximityGap.TwoPrimeWindowLaw
 import ArkLib.Data.CodingTheory.ProximityGap.TwoSidedBracketScale
