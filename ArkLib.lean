5959e8f4b7e3e2a8e38d443d353ad82b318087dc
lalalune <shawmakesmagic@gmail.com>
2026-06-14 18:42:56 -0700

    autosync: save __pushclean before main sync

--- ArkLib.lean
+++ ArkLib.lean
@@ -1582,10 +1582,8 @@ import ArkLib.Data.CodingTheory.ProximityGap.PencilDegreeBound
 import ArkLib.Data.CodingTheory.ProximityGap.PencilDegreeGeneral
 import ArkLib.Data.CodingTheory.ProximityGap.PencilNormalizerBand
 import ArkLib.Data.CodingTheory.ProximityGap.PencilSplitCount
-import ArkLib.Data.CodingTheory.ProximityGap.PeriodHistogramTailCount
 import ArkLib.Data.CodingTheory.ProximityGap.PeriodMomentLaw
 import ArkLib.Data.CodingTheory.ProximityGap.PeriodTailMarkov
-import ArkLib.Data.CodingTheory.ProximityGap.PeriodTailMarkovClosure
 import ArkLib.Data.CodingTheory.ProximityGap.PermanentlyBlocked
 import ArkLib.Data.CodingTheory.ProximityGap.PinBeyondJohnson
 import ArkLib.Data.CodingTheory.ProximityGap.PointwiseEnergyCeilingNoGo
