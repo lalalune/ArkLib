8ed480ef383a7161689ba16e431b80451fbf723c
Shaw <shawgotbags@gmail.com>
2026-07-10 23:19:38 -0400

    feat(#466 G145): bound lower-depth intersection multiplicity

--- ArkLib.lean
+++ ArkLib.lean
@@ -2242,6 +2242,7 @@ import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G141OffDiagonalAccidentId
 import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G142SubsetCollisionCancellationCore
 import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G143DepthStratifiedSubsetAccidents
 import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G144IntersectionAwareCancellationCode
+import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G145LowerDepthMultiplicityEnvelope
 import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G56AllDepthPatternDecomposition
 import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G57AllDepthWraparoundDCConsumer
 import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G58AllDepthLedgerReversal
