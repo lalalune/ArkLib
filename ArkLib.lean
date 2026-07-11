29f80e9d9f1585d65ed56785cb5254859570fa81
Shaw <shawgotbags@gmail.com>
2026-07-11 00:38:13 -0400

    feat(#466 G149): transfer primitive packet charges globally

--- ArkLib.lean
+++ ArkLib.lean
@@ -2246,6 +2246,7 @@ import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G145LowerDepthMultiplicit
 import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G146GlobalSubsetAccidentEnvelope
 import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G147ConnectedBalancedCoreRecursion
 import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G148FinitePrimitivePacketTree
+import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G149PrimitivePacketChargeTransfer
 import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G56AllDepthPatternDecomposition
 import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G57AllDepthWraparoundDCConsumer
 import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G58AllDepthLedgerReversal
