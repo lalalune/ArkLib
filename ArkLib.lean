0fecb3b8b245aa192d972ee365d63e327a18d066
Shaw <shawgotbags@gmail.com>
2026-07-10 22:36:24 -0400

    feat(#466 G142): canonicalize subset collision cores by depth

--- ArkLib.lean
+++ ArkLib.lean
@@ -2239,6 +2239,7 @@ import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G138CyclotomicLiftHandoff
 import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G139NoAccidentEntropyObstruction
 import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G140QuantitativeAccidentDCFloor
 import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G141OffDiagonalAccidentIdentity
+import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G142SubsetCollisionCancellationCore
 import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G56AllDepthPatternDecomposition
 import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G57AllDepthWraparoundDCConsumer
 import ArkLib.Data.CodingTheory.ProximityGap.Frontier._G58AllDepthLedgerReversal
